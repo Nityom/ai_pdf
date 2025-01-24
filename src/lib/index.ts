e77601f629fe338dd807f8e1a223ceff71bb4afb
Nityom Tikhe <nityomtikherr@gmail.com>
2025-01-24 15:29:24 +0530

    landing-page created

---
+++ src/lib/index.ts
@@ -0,0 +1,10 @@
+// place files you want to import through the `$lib` alias in this folder.
+
+export let seo = {
+	title: 'AI PDF Assist',
+	description:
+		'PDF Assistant: A streamlined tool to scrape, merge, extract, and query PDF content with AI-powered insights.',
+	url: 'https://startup-sve.vercel.app',
+
+};
+
